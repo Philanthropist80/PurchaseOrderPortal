d42e9ea3920029e931104c3be5f391b69b7c374e
Taimoor Adil Badshah <taimurad@hotmail.com>
2022-01-17 12:27:23 +0500

    remove unnecessory reference which casuing crashing issue. Microsoft.EntityFrameworkCore

--- Obermind.Operation/Obermind.Operation.Data.Model/Security/UserRole.cs
+++ Obermind.Operation/Obermind.Operation.Data.Model/Security/UserRole.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Obermind.Operation.Data.Model.Security
 {
